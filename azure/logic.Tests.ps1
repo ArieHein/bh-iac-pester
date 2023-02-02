371f87e0ce531285ead26a77138949d9a3855914
Arie Heinrich <arie.heinrich@outlook.com>
2023-02-02 20:38:09 +0100

    add Logic Apps tests

---
+++ azure/logic.Tests.ps1
@@ -0,0 +1,87 @@
+param (
+    [Parameter(Mandatory)][string]$SubscriptionName,
+    [Parameter(Mandatory)][string]$ResourceName,
+    [Parameter(Mandatory)][string]$ResourceGroupName,
+    [Parameter(Mandatory)][string]$ResourceLocation,
+    [Parameter(Mandatory)][hashtable]$ResourceTags
+)
+
+Describe "Azure Logic Apps" {
+    BeforeAll {
+        $Subscriptions = Get-AzContext -ListAvailable
+        foreach ($Subscription in $Subscriptions) {
+            if ($Subscription.SubscriptionName -eq $SubscriptionName) {
+                Set-AzContext -Subscription $SubscriptionName
+            }
+            else {
+                # Error out with Subscription Not Found. Note that by default only 25 Subscriptions
+                # will show up. Use Connect-AzAccount -MaxContextPopulation <int> to get more context
+            }
+        }
+
+        $ResourceGroups = Get-AzResourceGroup -Subscription $SubscriptionName
+
+        foreach ($ResourceGroup in $ResourceGroups) {
+
+            if ( -not ($ResourceGroup.Name -eq $ResourceGroupName)) {
+                # Error out with Resource Group Not Found.
+            }
+        }
+    }
+
+    Context "Resource Provision" {
+        # Get all the Logic Apps in the Resource Group
+        $Resources = Get-AzLogicApps -ResourceGroupName $ResourceGroupName
+
+        It "Logic App should exist in the expected Resource Group" {
+            $ResourceFound = $false
+
+            $Resources | ForEach-Object {
+                if (_$.Name -eq $ResourceName) {
+                    $ResourceFound = $true
+                    break
+                }
+            }
+            $ResourceFound | Should -Be $true
+        }
+
+        # Get specific Logic App
+        $Resource = Get-AzLogicApps -Name $ResourceName -ResourceGroupName $ResourceGroupName
+
+        It "Logic App should be in the expected Location" {
+            $Resource.Location | Should -Be $ResourceLocation
+        }
+
+        It "Logic App should have all the expected Resource Tags" {
+            $ResourceFound = $false
+            $Message = "Resource"
+            $CompareKeys = Compare-Object -ReferenceObject $Resource.Tags.Keys -DifferenceObject $ResourceTags.Keys
+            if ( -not ($CompareKeys)) {
+                $CompareValues = Compare-Object -ReferenceObject $Resource.Tags.Values -DifferenceObject $ResourceTags.Values
+                if ( -not ($CompareValues)) {
+                    $ResourceFound = $true
+                    $Message = $Message + " is Found."
+                }
+                else {
+                    $Message = $Message + " is not found. Keys equal, Values are not."
+                }
+            }
+            else {
+                $Message = $Message + " is not found. Keys are not equal."
+            }
+            $ResourceFound | Should -Be $true
+        }
+    }
+
+    Context "Resource Operation" {
+        # Get specific Logic App
+        $Resource = Get-AzLogicApps -Name $ResourceName -ResourceGroupName $ResourceGroupName
+
+        It "Logic App should be provisioned successfully" {
+            $Resource.ProvisioningState | Should -Be "Succeeded"
+        }
+    }
+
+    AfterAll {
+    }
+}
